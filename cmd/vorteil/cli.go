b11c6c555fc2e932738986f10faec06d7ba724da
Jens Gerke <jens.gerke@vorteil.io>
2020-09-17 07:34:50 +1000

    added help text

--- cmd/vorteil/cli.go
+++ cmd/vorteil/cli.go
@@ -1717,8 +1717,30 @@ func init() {
 }
 
 var convertContainerCmd = &cobra.Command{
-	Use:  "convert-container REPO:APP DESTFOLDER",
-	Args: cobra.ExactValidArgs(2),
+	Use:   "convert-container REPO:APP DESTFOLDER",
+	Args:  cobra.ExactValidArgs(2),
+	Short: "Convert containers into vortil.io virtual machines",
+	Long: `Convert containers into vorteil.io project folders. This command can convert
+containers from a remote repository as well as from local container runtimes.
+At the moment docker and containerd are supported.
+
+Local conversion examples:
+
+vorteil projects convert-container local.docker/nginx /target/directory
+vorteil projects convert-container local.containerd/docker.io/library/tomcat:latest /target/directory
+
+Remote conversion examples:
+
+./vorteil projects convert-container --config=/vconvert.yaml nginx /tmp/nginx
+
+The config file provided maps remote repository names to urls. If no file is provided
+docker.io, mcr.microsoft.com and gcr.io are automatically added. The following is an example
+config yaml:
+
+repositories:
+  myrepo:
+   url: https://myurl
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		// in case of an error we pass empty user/pwd/config in

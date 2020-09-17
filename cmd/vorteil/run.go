19a1757a581e56c6f7ed23ffa33a203ccab6c437
Trent Hilliam <trent.hilliam@vorteil.io>
2020-09-18 07:48:12 +1000

    test

--- cmd/vorteil/run.go
+++ cmd/vorteil/run.go
@@ -381,7 +381,7 @@ func raw(start bool) error {
 func listenForInterupt() (chan os.Signal, chan bool) {
 	var signalChannel chan os.Signal
 	signalChannel = make(chan os.Signal, 1)
-	signal.Notify(signalChannel, os.Interrupt, os.Kill)
+	signal.Notify(signalChannel, os.Interrupt)
 	chBool := make(chan bool, 1)
 
 	// check if this is running in a sygwin terminal, interupt signals are difficult to capture

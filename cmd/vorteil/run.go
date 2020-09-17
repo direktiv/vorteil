7d0ed891dce0c9c62b37c934ab86d85fdc12696f
Trent Hilliam <trent.hilliam@vorteil.io>
2020-09-18 08:50:58 +1000

    add sigterm to channel listener

--- cmd/vorteil/run.go
+++ cmd/vorteil/run.go
@@ -11,7 +11,6 @@ import (
 	"os/signal"
 	"path/filepath"
 	"runtime"
-	"syscall"
 	"time"
 
 	isatty "github.com/mattn/go-isatty"
@@ -385,7 +384,7 @@ func raw(start bool) error {
 func listenForInterupt() (chan os.Signal, chan bool) {
 	var signalChannel chan os.Signal
 	signalChannel = make(chan os.Signal, 1)
-	signal.Notify(signalChannel, os.Interrupt, os.Kill, syscall.SIGTERM)
+	signal.Notify(signalChannel, os.Interrupt, os.Kill, syscall.SIGTERM),
 	chBool := make(chan bool, 1)
 
 	// check if this is running in a sygwin terminal, interupt signals are difficult to capture

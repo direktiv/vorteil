cc367e233585b1d5b7c553e4a78d5eb8ad3a2f4f
Alan Murtagh <alan.murtagh@vorteil.io>
2020-09-21 14:43:13 +1000

    Fixes a few code smells in the ext logic.

--- pkg/ext/ext.go
+++ pkg/ext/ext.go
@@ -14,86 +14,9 @@ import (
 	"github.com/vorteil/vorteil/pkg/vio"
 )
 
-const (
-	SectorSize               = 512
-	BlockSize                = 0x1000
-	InodeSize                = 128
-	InodesPerBlock           = BlockSize / InodeSize
-	BlockGroupDescriptorSize = 32
-	blocksPerSuperblock      = 1
-	blocksPerBlockBitmap     = 1
-	blocksPerInodeBitmap     = 1
-
-	pointerSize         = 4
-	maxDirectPointers   = 12
-	dentryNameAlignment = 4
-
-	SuperUID                    = 1000
-	SuperGID                    = 1000
-	inodeDirectoryPermissions   = 0x4000 | 0700
-	inodeRegularFilePermissions = 0x8000 | 0700
-	inodeSymlinkPermissions     = 0xA000 | 0700
-)
-
-type Superblock struct {
-	TotalInodes         uint32
-	TotalBlocks         uint32
-	ReservedBlocks      uint32
-	UnallocatedBlocks   uint32
-	UnallocatedInodes   uint32
-	SuperblockNumber    uint32
-	BlockSize           uint32
-	FragmentSize        uint32
-	BlocksPerGroup      uint32
-	FragmentsPerGroup   uint32
-	InodesPerGroup      uint32
-	LastMountTime       uint32
-	LastWrittenTime     uint32
-	MountsSinceCheck    uint16
-	MountsCheckInterval uint16
-	Signature           uint16
-	State               uint16
-	ErrorProtocol       uint16
-	VersionMinor        uint16
-	TimeLastCheck       uint32
-	TimeCheckInterval   uint32
-	OS                  uint32
-	VersionMajor        uint32
-	SuperUser           uint16
-	SuperGroup          uint16
-}
-
-type Inode struct {
-	Permissions      uint16
-	UID              uint16
-	SizeLower        uint32
-	LastAccessTime   uint32
-	CreationTime     uint32
-	ModificationTime uint32
-	DeletionTime     uint32
-	GID              uint16
-	Links            uint16
-	Sectors          uint32
-	Flags            uint32
-	OSV              uint32
-	DirectPointer    [12]uint32
-	SinglyIndirect   uint32
-	DoublyIndirect   uint32
-	TriplyIndirect   uint32
-	GenNo            uint32
-	Reserved         [2]uint32
-	FragAddr         uint32
-	OSStuff          [12]byte
-}
-
-func divide(a, b int64) int64 {
-	return (a + b - 1) / b
-}
-
-func align(a, b int64) int64 {
-	return divide(a, b) * b
-}
-
+// CompilerArgs organizes all inputs necessary to create a new Compiler. Because
+// the compiler is designed to be configured in stages by the caller very little
+// goes here.
 type CompilerArgs struct {
 	FileTree vio.FileTree
 	Logger   elog.Logger
@@ -143,7 +66,11 @@ func NewCompiler(args *CompilerArgs) *Compiler {
 	return c
 }
 
+// Mkdir allows the caller to add an empty directory to the file-system at
+// 'path' if no file or directory is already mapped there. This function must
+// be called before calling Commit, otherwise the behaviour is undefined.
 func (c *Compiler) Mkdir(path string) error {
+
 	_, base := filepath.Split(path)
 	err := c.tree.Map(path, vio.CustomFile(vio.CustomFileArgs{
 		Name:  base,
@@ -152,10 +79,16 @@ func (c *Compiler) Mkdir(path string) error {
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
+// AddFile allows the caller to add a file to the file-system at 'path',
+// resolving any collisions by overwriting them if 'force' is true. This
+// function must be called before calling Commit, otherwise the behaviour is
+// undefined.
 func (c *Compiler) AddFile(path string, r io.ReadCloser, size int64, force bool) error {
+
 	_, base := filepath.Split(path)
 	err := c.tree.Map(path, vio.CustomFile(vio.CustomFileArgs{
 		Name:       base,
@@ -165,13 +98,20 @@ func (c *Compiler) AddFile(path string, r io.ReadCloser, size int64, force bool)
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
+// IncreaseMinimumInodes allows the caller to force in some extra empty inodes
+// on top of whatever would have otherwise been there. This function must be
+// called before calling Commit, otherwise the behaviour is undefined.
 func (c *Compiler) IncreaseMinimumInodes(inodes int64) {
 	c.minFreeInodes += inodes
 }
 
+// SetMinimumInodes allows the caller to specify the minimum number of inodes
+// that should be built onto the file-system. This function must be called
+// before calling Commit, otherwise the behaviour is undefined.
 func (c *Compiler) SetMinimumInodes(inodes int64) {
 	c.minInodes = inodes
 }

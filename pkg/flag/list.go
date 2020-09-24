d09a1aae0ab8c10d54cf02e5b3c360f33239e1e8
James <james.murtagh@vorteil.io>
2020-09-24 16:03:27 +1000

    Address some gofmt warnings in the flag package

--- pkg/flag/list.go
+++ pkg/flag/list.go
@@ -6,20 +6,24 @@ import (
 	"github.com/spf13/pflag"
 )
 
+// FlagsList contains an array of Flag objects
 type FlagsList []Flag
 
+// AddTo satisfies the Flag interface requirement
 func (f FlagsList) AddTo(flagSet *pflag.FlagSet) {
 	for _, x := range f {
 		x.AddTo(flagSet)
 	}
 }
 
+// AddUnhiddenTo satisfies the Flag interface requirement
 func (f FlagsList) AddUnhiddenTo(flagSet *pflag.FlagSet) {
 	for _, x := range f {
 		x.AddUnhiddenTo(flagSet)
 	}
 }
 
+// Validate satisfies the Flag interface requirement
 func (f FlagsList) Validate() error {
 	for _, x := range f {
 		err := x.FlagValidate()

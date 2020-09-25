2215b4089d5d807750b29d0ad5384604ac6fcf28
Trent Hilliam <trent.hilliam@vorteil.io>
2020-09-25 16:02:06 +1000

    move imageutil package

--- cmd/vorteil/images.go
+++ cmd/vorteil/images.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 
 	"github.com/spf13/cobra"
-	"github.com/vorteil/vorteil/cmd/vorteil/imageutil"
+	"github.com/vorteil/vorteil/pkg/imageutil"
 	"github.com/vorteil/vorteil/pkg/vdisk"
 	"github.com/vorteil/vorteil/pkg/vpkg"
 )

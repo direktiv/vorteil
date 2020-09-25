3345441843cdeb53d7180b8e0f21fabef6ad2faa
Trent Hilliam <trent.hilliam@vorteil.io>
2020-09-25 14:22:38 +1000

    Refactored LS Logic

--- cmd/vorteil/imageutil/ls.go
+++ cmd/vorteil/imageutil/ls.go
@@ -2,7 +2,7 @@ package imageutil
 
 import (
 	"fmt"
-	"os"
+	"log"
 	"path/filepath"
 	"strings"
 	"time"
@@ -12,9 +12,46 @@ import (
 	"github.com/vorteil/vorteil/pkg/vdecompiler"
 )
 
-func returnLSBoolFlags(cmd *cobra.Command) (bool, bool, bool, bool, error) {
-	var returnErr error
+func lsSetNumbers(cmd *cobra.Command) error {
+	numbers, err := cmd.Flags().GetString("numbers")
+	if err != nil {
+		return err
+	}
 
+	err = SetNumbersMode(numbers)
+	if err != nil {
+		return fmt.Errorf("couldn't parse value of --numbers: %v", err)
+	}
+	return nil
+}
+
+func printVPartition(iio *vdecompiler.IO, fpath string, long bool) error {
+	if fpath != "/" && fpath != "" && fpath != "." {
+		return fmt.Errorf("bad FILE_PATH for vorteil partition: %s", fpath)
+	}
+
+	kfiles, err := iio.KernelFiles()
+	if err != nil {
+		return err
+	}
+
+	if long {
+		var table [][]string
+		table = [][]string{{"", "", "", "", "", "", ""}}
+		for _, kf := range kfiles {
+			table = append(table, []string{"----------", "?", "-", "-", "-", fmt.Sprintf("%s", PrintableSize(kf.Size)), kf.Name})
+		}
+		PlainTable(table)
+	} else {
+		for _, kf := range kfiles {
+			log.Printf("%s", kf.Name)
+		}
+	}
+	return nil
+}
+
+func readLSBoolFlags(cmd *cobra.Command) (bool, bool, bool, bool, error) {
+	var returnErr error
 	all, err := cmd.Flags().GetBool("all")
 	if err != nil {
 		returnErr = err
@@ -34,76 +71,71 @@ func returnLSBoolFlags(cmd *cobra.Command) (bool, bool, bool, bool, error) {
 	if err != nil {
 		returnErr = err
 	}
-
 	return all, almostAll, long, recursive, returnErr
 }
 
-// LS list directory contents.
-func LS(log elog.View, cmd *cobra.Command, args []string, flagOS bool) error {
-
-	err := FetchNumberFlags(cmd)
-	if err != nil {
-		return err
-	}
-
-	var reiterating bool
-
-	all, almostAll, long, recursive, err := returnLSBoolFlags(cmd)
-	if err != nil {
-		return err
-	}
-
-	img := args[0]
-
+func readLS(fpath string, img string, long bool, flagOS bool) (*vdecompiler.IO, int, error) {
 	iio, err := vdecompiler.Open(img)
 	if err != nil {
+		return nil, 0, err
+	}
+
+	if flagOS {
+		err = printVPartition(iio, fpath, long)
+		if err != nil {
+			return nil, 0, err
+		}
+	}
+
+	ino, err := iio.ResolvePathToInodeNo(fpath)
+	if err != nil {
+		return nil, 0, err
+	}
+	return iio, ino, nil
+}
+
+// LS lists directory contents.
+func LS(log elog.View, cmd *cobra.Command, args []string, flagOS bool) error {
+
+	err := lsSetNumbers(cmd)
+	if err != nil {
 		return err
 	}
-	defer iio.Close()
 
-	var fpaths []string
-	var inos []int
-	var inodes []*vdecompiler.Inode
-	var table [][]string
-	var entries []*vdecompiler.DirectoryEntry
-
+	all, almostAll, long, recursive, err := readLSBoolFlags(cmd)
+	if err != nil {
+		return err
+	}
 	var fpath string
 	if len(args) > 1 {
 		fpath = args[1]
 	} else {
 		fpath = "/"
 	}
+	img := args[0]
 
-	if flagOS {
-		if fpath != "/" && fpath != "" && fpath != "." {
-			return fmt.Errorf("bad FILE_PATH for vorteil partition: %s", fpath)
-		}
-
-		kfiles, err := iio.KernelFiles()
-		if err != nil {
-			return err
-		}
-
-		if long {
-			var table [][]string
-			table = [][]string{{"", "", "", "", "", "", ""}}
-			for _, kf := range kfiles {
-				table = append(table, []string{"----------", "?", "-", "-", "-", fmt.Sprintf("%s", PrintableSize(kf.Size)), kf.Name})
-			}
-			PlainTable(table)
-		} else {
-			for _, kf := range kfiles {
-				log.Printf("%s", kf.Name)
-			}
-		}
-		return nil
+	iio, ino, err := readLS(fpath, img, long, flagOS)
+	if err != nil {
+		return err
 	}
+	defer iio.Close()
 
-	ino, err := iio.ResolvePathToInodeNo(fpath)
+	err = readInodesLS(log, iio, ino, long, recursive, all, almostAll, fpath)
 	if err != nil {
 		return err
 	}
 
+	return nil
+}
+
+func readInodesLS(log elog.View, iio *vdecompiler.IO, ino int, long, recursive, all, almostAll bool, fpath string) error {
+	var reiterating bool
+
+	var fpaths []string
+	var inos []int
+	var inodes []*vdecompiler.Inode
+	var table [][]string
+	var entries []*vdecompiler.DirectoryEntry
 inoEntry:
 	inode, err := iio.ResolveInode(ino)
 	if err != nil {
@@ -152,8 +184,7 @@ inodeEntry:
 		if long {
 			child, err := iio.ResolveInode(entry.Inode)
 			if err != nil {
-				log.Errorf("%v", err)
-				os.Exit(1)
+				return err
 			}
 			links := "?"
 

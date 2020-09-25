9b40f47c47055fd1f7ade7e19034b820eecdf381
jon <jon.alfaro@vorteil.io>
2020-09-25 11:15:36 +1000

    config realted logic moved from main.go to conf.go

--- cmd/vorteil/main.go
+++ cmd/vorteil/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"errors"
 	"fmt"
 	"io"
@@ -12,16 +11,12 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/mitchellh/go-homedir"
 	"github.com/sisatech/tablewriter"
-	"github.com/sisatech/toml"
 	"github.com/spf13/pflag"
 	"github.com/vorteil/vorteil/pkg/vcfg"
 	"github.com/vorteil/vorteil/pkg/vdecompiler"
 	"github.com/vorteil/vorteil/pkg/vdisk"
-	"github.com/vorteil/vorteil/pkg/vimg"
 	"github.com/vorteil/vorteil/pkg/vio"
-	"github.com/vorteil/vorteil/pkg/vkern"
 	"github.com/vorteil/vorteil/pkg/vpkg"
 	"github.com/vorteil/vorteil/pkg/vproj"
 )
@@ -97,109 +92,6 @@ func parseImageFormat(s string) (vdisk.Format, error) {
 		return format, fmt.Errorf("%w -- try one of these: %s", err, strings.Join(vdisk.AllFormatStrings(), ", "))
 	}
 	return format, nil
-}
-
-type vorteildConf struct {
-	KernelSources struct {
-		Directory          string   `toml:"directory"`
-		DropPath           string   `toml:"drop-path"`
-		RemoteRepositories []string `toml:"remote-repositories"`
-	} `toml:"kernel-sources"`
-}
-
-var ksrc vkern.Manager
-
-type vorteilConfig struct {
-	kernels string
-	watch   string
-	sources []string
-}
-
-// loadVorteilConfig : Load vorteil config from ~/.vorteild path.
-//	If this path fails to load, load defaults instead.
-//	Return vorteilConfig Objects with loaded config values
-func loadVorteilConfig() (vorteilConfig, error) {
-	var vCfg vorteilConfig
-
-	home, err := homedir.Dir()
-	if err != nil {
-		return vCfg, err
-	}
-
-	vorteild := filepath.Join(home, ".vorteild")
-	conf := filepath.Join(vorteild, "conf.toml")
-
-	confData, err := ioutil.ReadFile(conf)
-	if err != nil {
-		vCfg.kernels = filepath.Join(vorteild, "kernels")
-		vCfg.watch = filepath.Join(vCfg.kernels, "watch")
-		vCfg.sources = []string{"https://downloads.vorteil.io/kernels"}
-	} else {
-		vconf := new(vorteildConf)
-		err = toml.Unmarshal(confData, vconf)
-		if err != nil {
-			return vCfg, err
-		}
-		vCfg.kernels = vconf.KernelSources.Directory
-		vCfg.watch = vconf.KernelSources.DropPath
-		vCfg.sources = vconf.KernelSources.RemoteRepositories
-	}
-
-	return vCfg, nil
-}
-
-// mkDirAllSlice - Utils : Create the directories in the 'dirs' slice with permissions of 'perm'
-func mkDirAllSlice(perm os.FileMode, dirs ...string) error {
-	for _, dir := range dirs {
-		err := os.MkdirAll(dir, perm)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func constructGetLastestKernelsFunc(ksrc vkern.Manager) func(ctx context.Context) (vkern.CalVer, error) {
-	return func(ctx context.Context) (vkern.CalVer, error) {
-		s, err := ksrc.Latest()
-		if err != nil {
-			return vkern.CalVer(""), err
-		}
-		k, err := vkern.Parse(s)
-		if err != nil {
-			return vkern.CalVer(""), err
-		}
-		return k, nil
-	}
-}
-
-func initKernels() error {
-	vCfg, err := loadVorteilConfig()
-	if err != nil {
-		return err
-	}
-
-	err = mkDirAllSlice(0777, vCfg.kernels, vCfg.watch)
-	if err != nil {
-		return err
-	}
-
-	ksrc, err = vkern.CLI(vkern.CLIArgs{
-		Directory:          vCfg.kernels,
-		DropPath:           vCfg.watch,
-		RemoteRepositories: vCfg.sources,
-	}, log)
-	if err != nil {
-		return err
-	}
-
-	vkern.Global = ksrc
-	vimg.GetKernel = ksrc.Get
-	vimg.GetLatestKernel = constructGetLastestKernelsFunc(ksrc)
-
-	return nil
-
 }
 
 type sourceType string

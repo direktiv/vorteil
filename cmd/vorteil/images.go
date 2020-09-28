79eea2f5b5242a31744277b41679dfa97ce6c03a
jon <jon.alfaro@vorteil.io>
2020-09-28 13:24:46 +1000

    statCmd moved to stat.go

--- cmd/vorteil/images.go
+++ cmd/vorteil/images.go
@@ -982,107 +982,38 @@ var statCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		numbers, err := cmd.Flags().GetString("numbers")
 		if err != nil {
-			panic(err)
+			log.Errorf("%v", err)
+			os.Exit(1)
 		}
 
 		err = SetNumbersMode(numbers)
 		if err != nil {
 			log.Errorf("couldn't parse value of --numbers: %v", err)
-			return
+			os.Exit(1)
 		}
 
 		img := args[0]
-
-		iio, err := vdecompiler.Open(img)
-		if err != nil {
-			log.Errorf("%v", err)
-			os.Exit(1)
-		}
-		defer iio.Close()
 
 		fpath := "/"
 		if len(args) > 1 {
 			fpath = args[1]
 		}
 
-		if flagOS {
-			var s string
-			var size int
-			ftype := "regular file"
-
-			fpath = strings.TrimPrefix(fpath, "/")
-			if fpath == "" {
-				s = "/"
-				size = 0
-			} else {
-				kfiles, err := iio.KernelFiles()
-				if err != nil {
-					log.Errorf("%v", err)
-					os.Exit(1)
-				}
-
-				for _, kf := range kfiles {
-					if kf.Name == fpath {
-						s = fpath
-						size = kf.Size
-						break
-					}
-				}
-
-				if s == "" {
-					log.Errorf("kernel file not found: %s", fpath)
-					os.Exit(1)
-				}
-			}
-
-			log.Printf("File: %s\t%s", s, ftype)
-			log.Printf("Size: %s", PrintableSize(size))
-			log.Printf("Inode: -")
-			log.Printf("Access: -")
-			log.Printf("Uid: -")
-			log.Printf("Gid: -")
-			log.Printf("Access: -")
-			log.Printf("Modify: -")
-			log.Printf("Create: -")
-
-		} else {
-			ino, err := iio.ResolvePathToInodeNo(fpath)
-			if err != nil {
-				log.Errorf("%v", err)
-				os.Exit(1)
-			}
-
-			inode, err := iio.ResolveInode(ino)
-			if err != nil {
-				log.Errorf("%v", err)
-				os.Exit(1)
-			}
-
-			var ftype string
-
-			var user, group string
-			user = "?"
-			group = "?"
-			if inode.UID == vdecompiler.VorteilUserID {
-				user = vdecompiler.VorteilUserName
-			}
-			if inode.GID == vdecompiler.VorteilGroupID {
-				group = vdecompiler.VorteilGroupName
-			}
-
-			log.Printf("File: %s\t%s", filepath.Base(fpath), ftype)
-			log.Printf("Size: %s", PrintableSize(inode.Fullsize()))
-			// TODO: log.Printf("Blocks: %s", PrintableSize(int()))
-			// TODO: log.Printf("IO Block: %s", PrintableSize())
-			log.Printf("Inode: %d", ino)
-			// TODO: log.Printf("Links: %s")
-			log.Printf("Access: %#o/%s", inode.Mode&vdecompiler.InodePermissionsMask, inode.Permissions())
-			log.Printf("Uid: %d (%s)", inode.UID, user)
-			log.Printf("Gid: %d (%s)", inode.GID, group)
-			log.Printf("Access: %s", time.Unix(int64(inode.LastAccessTime), 0))
-			log.Printf("Modify: %s", time.Unix(int64(inode.ModificationTime), 0))
-			log.Printf("Create: %s", time.Unix(int64(inode.CreationTime), 0))
+		fileStat, err := imageUtils.StatImageFile(img, fpath, flagOS)
+		if err != nil {
+			log.Errorf("%v", err)
+			os.Exit(1)
 		}
+
+		log.Printf("File: %s", fileStat.FileName)
+		log.Printf("Size: %s", PrintableSize(fileStat.Size))
+		log.Printf("Inode: %d", fileStat.Inode)
+		log.Printf("Permissions: %s", fileStat.Permissions)
+		log.Printf("Uid: %d (%s)", fileStat.UID, fileStat.User)
+		log.Printf("Gid: %d (%s)", fileStat.GID, fileStat.Group)
+		log.Printf("Access: %s", fileStat.Access)
+		log.Printf("Modify: %s", fileStat.Modify)
+		log.Printf("Create: %s", fileStat.Create)
 	},
 }
 
---
+++ pkg/imageUtils/stat.go
@@ -0,0 +1,109 @@
+package imageUtils
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/vorteil/vorteil/pkg/vdecompiler"
+)
+
+// TODO StatFileReport - Blocks
+// TODO StatFileReport - IO Block
+// TODO StatFileReport - Links
+
+// StatFileReport ...
+type StatFileReport struct {
+	FileName    string
+	FileType    string
+	Size        int
+	Inode       int
+	UID         uint16
+	User        string
+	GID         uint16
+	Group       string
+	Permissions string
+	Access      time.Time
+	Modify      time.Time
+	Create      time.Time
+}
+
+// StatImageFile ...
+func StatImageFile(vorteilImagePath string, imageFilePath string, seekOS bool) (StatFileReport, error) {
+	var statOut StatFileReport
+	vorteilImage, err := vdecompiler.Open(vorteilImagePath)
+	if err != nil {
+		return statOut, err
+	}
+	defer vorteilImage.Close()
+
+	if seekOS {
+		var s string
+		var size int
+		ftype := "regular file"
+
+		imageFilePath = strings.TrimPrefix(imageFilePath, "/")
+		if imageFilePath == "" {
+			s = "/"
+			size = 0
+		} else {
+			kfiles, err := vorteilImage.KernelFiles()
+			if err != nil {
+				return statOut, err
+			}
+
+			for _, kf := range kfiles {
+				if kf.Name == imageFilePath {
+					s = imageFilePath
+					size = kf.Size
+					break
+				}
+			}
+
+			if s == "" {
+				return statOut, err
+			}
+		}
+
+		statOut.FileName = s
+		statOut.FileType = ftype
+		statOut.Size = size
+	} else {
+		ino, err := vorteilImage.ResolvePathToInodeNo(imageFilePath)
+		if err != nil {
+			return statOut, err
+		}
+
+		inode, err := vorteilImage.ResolveInode(ino)
+		if err != nil {
+			return statOut, err
+		}
+
+		var ftype string
+
+		var user, group string
+		user = "?"
+		group = "?"
+		if inode.UID == vdecompiler.VorteilUserID {
+			user = vdecompiler.VorteilUserName
+		}
+		if inode.GID == vdecompiler.VorteilGroupID {
+			group = vdecompiler.VorteilGroupName
+		}
+
+		statOut.FileName = filepath.Base(imageFilePath)
+		statOut.FileType = ftype
+		statOut.Size = inode.Fullsize()
+		statOut.Inode = ino
+		statOut.Permissions = fmt.Sprintf("%#o/%s", inode.Mode&vdecompiler.InodePermissionsMask, inode.Permissions())
+		statOut.UID = inode.UID
+		statOut.User = user
+		statOut.GID = inode.GID
+		statOut.Group = group
+		statOut.Access = time.Unix(int64(inode.LastAccessTime), 0)
+		statOut.Modify = time.Unix(int64(inode.ModificationTime), 0)
+		statOut.Create = time.Unix(int64(inode.CreationTime), 0)
+	}
+	return statOut, err
+}

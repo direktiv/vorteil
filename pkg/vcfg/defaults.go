93e7050710b3ef12c7a4f25d259ca6ef5c2d524c
James <james.murtagh@vorteil.io>
2020-10-14 11:49:21 +1000

    Set default hostnames if empty

--- pkg/vcfg/defaults.go
+++ pkg/vcfg/defaults.go
@@ -6,6 +6,10 @@ package vcfg
  */
 
 import (
+	"fmt"
+	"regexp"
+	"strings"
+
 	"github.com/vorteil/vorteil/pkg/elog"
 )
 
@@ -28,5 +32,24 @@ func WithDefaults(v *VCFG, logger elog.View) error {
 		v.VM.CPUs = 1
 	}
 
+	if v.System.Hostname == "" {
+		if v.Info.Name != "" {
+			v.System.Hostname = fmt.Sprintf("%s-$SALT", sanitizeHostname(v.Info.Name))
+		} else {
+			v.System.Hostname = "vorteil-$SALT"
+		}
+		logger.Debugf("Setting empty hostname field to '%s'", v.System.Hostname)
+	}
+
 	return nil
+}
+
+var hostnameRegexp = regexp.MustCompile(`(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])`)
+
+func sanitizeHostname(s string) string {
+	x := hostnameRegexp.FindAllString(s, -1)
+	if x == nil {
+		return "vorteil"
+	}
+	return strings.Join(x, "")
 }

0f387aef14a13f1424ce196b21db9ec1d1a92e73
Jon Alfaro <jon.alfaro@vorteil.io>
2020-10-29 09:16:22 +1000

    comments removed
    Signed-off-by: Jon Alfaro <jon.alfaro@vorteil.io>

--- cmd/vorteil/main.go
+++ cmd/vorteil/main.go
@@ -8,26 +8,6 @@ import (
 var log elog.View
 
 func main() {
-	// logger := &elog.CLI{}
-	// log = logger
-
-	// gcpCFG := google.Config{
-	// 	Bucket: "GCP BUCKET",
-	// 	Key:    "GCP KEY",
-	// }
-
-	// gcpData, errZ := json.Marshal(gcpCFG)
-	// if errZ != nil {
-	// 	panic(errZ)
-	// }
-
-	// googleProvisioner, errZ := registry.NewProvisioner("google", log, gcpData)
-	// if errZ != nil {
-	// 	panic(errZ)
-	// }
-
-	// googleProvisioner.Provision(&provisioners.ProvisionArgs{})
-	// os.Exit(1)
 	cli.InitializeCommands()
 
 	err := cli.RootCommand.Execute()
